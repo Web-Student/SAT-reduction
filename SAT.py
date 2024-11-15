566f9abc278f51c138b0202b4540c63c5e8fb0ce
reall <reall@MSI>
2024-11-15 15:52:22 -0700

    try 2 at reduction, sudo code for and or or gates

--- SAT.py
+++ SAT.py
@@ -21,6 +21,30 @@ problem = LpProblem('SAT')
 for var in variables.keys():
     problem += (variables[var] == 0 or variables[var] == 1)
 
+# define or constraints (split on semicolon)
+separatedOrs = input.split(';')
+#for item in separatedOrs:
+    # problem += output >= parseAnyNot(item1)
+    # problem += output >= parseAnyNot(item2)
+    # problem += output <= parseAnyNot(item1) + parseAnyNot(item2)
+
+
+
+
+# define not constraints
+#find every variable with a dash right in front of it and add a - to it
+#to explore the possiblity of adding a not to it, let's save all of these constraints to a list for now as well
+notConstraints = []
+split_on_dashes = input.split('-')
+for segment in split_on_dashes:
+    notVar = segment[0]
+    newLPProblem = (1 - variables[notVar])
+    problem += newLPProblem
+    notConstraints.append(newLPProblem)
+
+
+#define constraints for everything being ored
+
 
 #problem += booleanLogic.GetSATResult(variables) == True #except, does it want the list of variables itself, or really just each of the letters it represents?
 
